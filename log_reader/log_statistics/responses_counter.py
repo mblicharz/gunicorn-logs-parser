ea2ec9cef68e251a5363a833aac38a362ece7a6f
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-15 19:58:07 +0200

    Add get_output method for ResponsesCounter

--- log_reader/log_statistics/responses_counter.py
+++ log_reader/log_statistics/responses_counter.py
@@ -1,2 +1,10 @@
-class ResponsesCounter:
-    pass
+from collections import Counter
+
+
+class ResponsesCounter(Counter):
+    def __init__(self):
+        super().__init__()
+
+    def get_output(self):
+        output = ', '.join(f'{k}: {v}' for k, v in sorted(self.items()))
+        return f'({output})'
--- tests/test_responses_counter.py
+++ tests/test_responses_counter.py
@@ -9,6 +9,12 @@ def responses_counter_obj():
 
 
 def test_ResponsesCounter_for_proper_output(responses_counter_obj):
+    responses_counter_obj.update({200: 1, 201: 2, 404: 4})
+    output = responses_counter_obj.get_output()
+    assert output == '(200: 1, 201: 2, 404: 4)'
+
+
+def test_ResponsesCounter_for_proper_order_of_output(responses_counter_obj):
     responses_counter_obj.update({201: 2, 200: 1, 404: 4})
     output = responses_counter_obj.get_output()
     assert output == '(200: 1, 201: 2, 404: 4)'
