ab08acbb91b7e0298047f065b74b8a33e8f71aba
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-15 16:33:53 +0200

    Add data assignation to LineReader fields

--- log_reader/line_reader.py
+++ log_reader/line_reader.py
@@ -1,13 +1,23 @@
 import re
 from typing import Sequence
 
-VALID_LOG_FORMAT = r'([(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (\d+) "(.*?)" "(.*?)" (\d+)'
+VALID_LOG_FORMAT = r'([(\d\.)]+) - (.*?) \[(.*?)\] "(.*?)" (\d+) (\d+) "(.*?)" "(.*?)" (\d+)'
 
 
 class LineReader:
     def __init__(self, line: str):
         self.line = line
         self.grouped_line = self._group_line(line)
+
+        self.remote_address = self.grouped_line[0]
+        self.user_name = self.grouped_line[1]
+        self.date = self.grouped_line[2]
+        self.status_line = self.grouped_line[3]
+        self.status_code = self.grouped_line[4]
+        self.response_len = self.grouped_line[5]
+        self.referer = self.grouped_line[6]
+        self.user_agent = self.grouped_line[7]
+        self.time = self.grouped_line[8]
 
     def _group_line(self, line: str) -> Sequence[str]:
         match = re.match(VALID_LOG_FORMAT, line)
