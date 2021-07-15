5256b0f6073f7696118541802e930ab6bebf3e17
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-15 16:27:18 +0200

    Fix in tests for LineReader

--- tests/tools.py
+++ tests/tools.py
@@ -5,7 +5,7 @@ def get_log_file():
 def get_log_line_with_grouped_output():
     return {
         'output': (
-            '172.16.3.14', '01/Dec/2019:11:06:05 +0100',
+            '172.16.3.14', '-', '01/Dec/2019:11:06:05 +0100',
             'GET /internal/user/38ca8008-fb70-44f0-a356-9bb9192ba15c/agenda/2019-12-01/2019-12-02 HTTP/1.1',
             '200', '720', '-', 'python-requests/2.22.0',
             '276202'),
