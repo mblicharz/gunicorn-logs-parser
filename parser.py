b2fbe8ed3a530c782fa214117b51a5684f0795e8
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-15 18:40:15 +0200

    Clean parser.py

--- parser.py
+++ parser.py
@@ -12,10 +12,3 @@ parser.add_argument('logfile', type=str)
 args = parser.parse_args()
 
 log_reader = LogReader(args.logfile, args.From, args.To)
-
-counter = 0
-
-for lines in log_reader:
-    counter += 1
-
-print(f'requests: {counter}')
