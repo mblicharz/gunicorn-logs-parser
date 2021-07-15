c016a89898b7cbf96df68298118048a1f13eaddb
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-15 15:56:12 +0200

    Add blank LineReader class

--- log_reader/__init__.py
+++ log_reader/__init__.py
@@ -1,4 +1,5 @@
 from .log_reader import LogReader
 from .datetime_parser import parse_to_datetime
+from .line_reader import LineReader
 
-__all__ = ['LogReader', 'parse_to_datetime']
+__all__ = ['LogReader', 'LineReader', 'parse_to_datetime']
---
+++ log_reader/line_reader.py
@@ -0,0 +1,2 @@
+class LineReader:
+    pass
