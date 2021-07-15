657fe6c83b07cb1e553fe8421cc6794e464cf731
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-15 16:12:40 +0200

    Add constructor with validating line in LineReader

--- log_reader/line_reader.py
+++ log_reader/line_reader.py
@@ -1,2 +1,16 @@
+import re
+from typing import Sequence
+
+VALID_LOG_FORMAT = r'([(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (\d+) "(.*?)" "(.*?)" (\d+)'
+
+
 class LineReader:
-    pass
+    def __init__(self, line: str):
+        self.line = line
+        self.grouped_line = self._group_line(line)
+
+    def _group_line(self, line: str) -> Sequence[str]:
+        match = re.match(VALID_LOG_FORMAT, line)
+        if not match:
+            raise ValueError
+        return match.groups()
