b54380abcf5b037af083127bc9309919c086865e
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-15 19:45:20 +0200

    Add tests for ResponsesCounter

---
+++ tests/test_responses_counter.py
@@ -0,0 +1,14 @@
+import pytest
+
+from log_reader import ResponsesCounter
+
+
+@pytest.fixture()
+def responses_counter_obj():
+    return ResponsesCounter()
+
+
+def test_ResponsesCounter_for_proper_output(responses_counter_obj):
+    responses_counter_obj.update({201: 2, 200: 1, 404: 4})
+    output = responses_counter_obj.get_output()
+    assert output == '(200: 1, 201: 2, 404: 4)'
