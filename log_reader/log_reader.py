b74cca98bdcc0c19ea3bb398a2d89e72f56d051a
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-16 13:22:59 +0200

    Use LogLine obj instead str in LogReader

--- log_reader/log_reader.py
+++ log_reader/log_reader.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from pathlib import Path
+from typing import Iterator
 
+from .log_line import LogLine
 from .datetime_parser import parse_to_datetime_without_timezone
 
 
@@ -32,7 +34,7 @@ class LogReader:
         if not Path(path).is_file():
             raise FileNotFoundError
 
-    def _read_file(self):
+    def _read_file(self) -> Iterator:
         with open(self.file) as file_lines:
             next(file_lines)
 
@@ -46,23 +48,24 @@ class LogReader:
                 if not edge_date_exists or self._is_in_date_range(log_line):
                     yield log_line
 
-    def _set_first_and_last_date(self, log_line: str) -> None:
+    def _set_first_and_last_date(self, log_line: LogLine) -> None:
         if not self.last_date:
-            self.last_date = self._get_datetime(log_line)
-        self.first_date = self._get_datetime(log_line)
+            self.last_date = parse_to_datetime_without_timezone(log_line.date)
+        self.first_date = parse_to_datetime_without_timezone(log_line.date)
 
     @staticmethod
-    def _extract_log_line(file_line: str) -> str:
-        return file_line.split(': ')[1]
+    def _extract_log_line(file_line: str) -> LogLine:
+        log_line = file_line.split(': ')[1]
+        return LogLine(log_line)
 
-    def _is_in_date_range(self, log_line: str) -> bool:
-        log_date = self._get_datetime(log_line)
+    def _is_in_date_range(self, log_line: LogLine) -> bool:
+        dt = parse_to_datetime_without_timezone(log_line.date)
 
-        if not log_date:
+        if not dt:
             return False
 
-        in_range = not self.from_date or self.from_date <= log_date \
-            and not self.to_date or self.to_date >= log_date
+        in_range = (not self.from_date or self.from_date <= dt) \
+            and (not self.to_date or self.to_date >= dt)
 
         return in_range
 
