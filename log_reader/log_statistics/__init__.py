091f9a3b74ff88dd3033b664cb56913e7c0ccb3c
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-16 18:24:33 +0200

    Fix imports in log_statistics

--- log_reader/log_statistics/__init__.py
+++ log_reader/log_statistics/__init__.py
@@ -1,7 +1,3 @@
-from .request_counter import RequestCounter
-from .response_counter import ResponseCounter
-from .average_counter import AverageCounter
 from .statistic_collector import StatisticCollector
 
-__all__ = ['RequestCounter', 'ResponseCounter', 'AverageCounter',
-           'StatisticCollector']
+__all__ = ['StatisticCollector']
