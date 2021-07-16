1458f86f11848bb6f67538ae72ebf88208184eae
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-16 21:36:04 +0200

    Refactor LogReader

--- log_reader/log_reader.py
+++ log_reader/log_reader.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, Optional
 
 from .log_line import LogLine
 from .datetime_parser import parse_to_datetime_without_timezone
 
 
 class LogReader:
-    def __init__(self, file_path: str, from_date: datetime, to_date: datetime) -> None:
+    def __init__(
+            self,
+            file_path: str,
+            from_date: Optional[datetime],
+            to_date: Optional[datetime]
+    ) -> None:
         self._validate_path(file_path)
 
         self.file = file_path
@@ -34,14 +39,25 @@ class LogReader:
             next(file_lines)
 
             for file_line in file_lines:
-                log_line = self._extract_log_line(file_line)
+                try:
+                    yield self._get_log_line(file_line)
+                except ValueError:
+                    continue
 
-                self._set_first_and_last_date(log_line)
+    def _get_log_line(self, line: str) -> Optional[LogLine]:
+        try:
+            log_line = self._extract_log_line(line)
+        except ValueError:
+            return None
 
-                edge_date_exists = self.from_date or self.to_date
+        self._set_first_and_last_date(log_line)
 
-                if not edge_date_exists or self._is_in_date_range(log_line):
-                    yield log_line
+        edge_date_exists = self.from_date or self.to_date
+
+        if not edge_date_exists or self._is_in_date_range(log_line):
+            return log_line
+        else:
+            raise ValueError
 
     def _set_first_and_last_date(self, log_line: LogLine) -> None:
         if not self.last_date:
@@ -59,8 +75,7 @@ class LogReader:
         if not dt:
             return False
 
-        in_range = (not self.from_date or self.from_date <= dt) and (
-            not self.to_date or self.to_date >= dt
-        )
+        dt_meets_from_date = not self.from_date or self.from_date <= dt
+        dt_meets_to_date = not self.to_date or self.to_date >= dt
 
-        return in_range
+        return dt_meets_from_date and dt_meets_to_date
