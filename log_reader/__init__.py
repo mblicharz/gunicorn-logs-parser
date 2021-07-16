d8f2c545db31d9e2717682877ff7755948bbbada
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-16 15:00:02 +0200

    Add empty statistic_collector.py

--- log_reader/__init__.py
+++ log_reader/__init__.py
@@ -2,6 +2,7 @@ from .log_reader import LogReader
 from .datetime_parser import parse_to_datetime
 from .log_line import LogLine
 from .log_statistics import RequestCounter, ResponseCounter, AverageCounter
+from .log_statistics import statistic_collector
 
 __all__ = ['LogReader', 'LogLine', 'parse_to_datetime', 'RequestCounter',
-           'ResponseCounter', 'AverageCounter']
+           'ResponseCounter', 'AverageCounter', 'statistic_collector']
--- log_reader/log_statistics/__init__.py
+++ log_reader/log_statistics/__init__.py
@@ -1,5 +1,7 @@
 from .request_counter import RequestCounter
 from .response_counter import ResponseCounter
 from .average_counter import AverageCounter
+from . import statistic_collector
 
-__all__ = ['RequestCounter', 'ResponseCounter', 'AverageCounter']
+__all__ = ['RequestCounter', 'ResponseCounter', 'AverageCounter',
+           'statistic_collector']
