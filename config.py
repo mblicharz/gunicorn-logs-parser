0c79543c0f3a3d41b61170214dc2624fd3cb8f1e
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-18 18:17:31 +0200

    Update on config.py

--- config.py
+++ config.py
@@ -1,1 +1,22 @@
+from gunicorn_log_parser.statistics import RequestsCount
+from gunicorn_log_parser.statistics import ResponsesCount
+
 log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
+
+statistics = {
+    'requests_count': {
+        'active': True,
+        'class': RequestsCount,
+        'placeholder': 'req_count'
+    },
+    'responses_count': {
+        'active': True,
+        'class': ResponsesCount,
+        'placeholder': 'res_count'
+    }
+}
+
+output_template = '''
+Requests: {req_count}
+Responses: {res_count}
+'''
